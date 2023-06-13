9f46b8ed42718e6573684de8657e4d521d62f53e
Christian Borst <christianoborst@gmail.com>
2023-06-13 14:26:06 -0400

    Remove dependency on cometbft by copying code
    Unfortunately Go's tooling for managing repo fork replacements like
    tendermint with cometbft does not handle things like a dependency on
    github.com/tendermint/tendermint/cmd/tendermint, since a replace
    directive resolves to github.com/cometbft/cometbft/cmd/tendermint, which
    does not exist in the cometbft repo. This in turn causes dependency
    issues and multiple version requirements in places like the althea-chain
    repo.
    
    In short, I have unfortunately copied much from
    github.com/cometbft/cometbft/cmd/cometbft/commands to temporarily
    alleviate this issue while we still use the v0.45.x line of the sdk.

--- server/util.go
+++ server/util.go
@@ -1,24 +1,33 @@
 package server
 
 import (
+	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/evmos/ethermint/server/config"
 	"github.com/gorilla/mux"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"golang.org/x/net/netutil"
 
 	sdkserver "github.com/cosmos/cosmos-sdk/server"
 	"github.com/cosmos/cosmos-sdk/server/types"
 	"github.com/cosmos/cosmos-sdk/version"
 
-	tmcmd "github.com/cometbft/cometbft/cmd/cometbft/commands"
+	tmcfg "github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/libs/cli"
 	tmlog "github.com/tendermint/tendermint/libs/log"
+	tmos "github.com/tendermint/tendermint/libs/os"
+	"github.com/tendermint/tendermint/privval"
 	rpcclient "github.com/tendermint/tendermint/rpc/jsonrpc/client"
 )
+
+var logger = tmlog.NewTMLogger(tmlog.NewSyncWriter(os.Stdout))
 
 // AddCommands adds server commands
 func AddCommands(rootCmd *cobra.Command, defaultNodeHome string, appCreator types.AppCreator, appExport types.AppExporter, addStartFlags types.ModuleInitFlags) {
@@ -32,8 +41,8 @@ func AddCommands(rootCmd *cobra.Command, defaultNodeHome string, appCreator type
 		sdkserver.ShowValidatorCmd(),
 		sdkserver.ShowAddressCmd(),
 		sdkserver.VersionCmd(),
-		tmcmd.ResetAllCmd,
-		tmcmd.ResetStateCmd,
+		ResetAllCmd,
+		ResetStateCmd,
 	)
 
 	startCmd := StartCmd(appCreator, defaultNodeHome)
@@ -118,4 +127,207 @@ func Listen(addr string, config *config.Config) (net.Listener, error) {
 		ln = netutil.LimitListener(ln, config.JSONRPC.MaxOpenConnections)
 	}
 	return ln, err
+}
+
+//==========================================================================
+//	NOTICE: the following code has been copied from
+//  github.com/cometbft/cometbft/cmd/cometbft/commands due to issues
+//  with replacing tendermint for cometbft and versioning complications
+//==========================================================================
+
+// ParseConfig retrieves the default environment configuration,
+// sets up the CometBFT root and ensures that the root exists
+func ParseConfig(cmd *cobra.Command) (*tmcfg.Config, error) {
+	conf := tmcfg.DefaultConfig()
+	err := viper.Unmarshal(conf)
+	if err != nil {
+		return nil, err
+	}
+
+	var home string
+	if os.Getenv("CMTHOME") != "" {
+		home = os.Getenv("CMTHOME")
+	} else if os.Getenv("TMHOME") != "" {
+		// XXX: Deprecated.
+		home = os.Getenv("TMHOME")
+		logger.Error("Deprecated environment variable TMHOME identified. CMTHOME should be used instead.")
+	} else {
+		home, err = cmd.Flags().GetString(cli.HomeFlag)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	conf.RootDir = home
+
+	conf.SetRoot(conf.RootDir)
+	tmcfg.EnsureRoot(conf.RootDir)
+	if err := conf.ValidateBasic(); err != nil {
+		return nil, fmt.Errorf("error in config file: %v", err)
+	}
+
+	return conf, nil
+}
+
+// ResetAllCmd removes the database of this CometBFT core
+// instance.
+var ResetAllCmd = &cobra.Command{
+	Use:     "unsafe-reset-all",
+	Aliases: []string{"unsafe_reset_all"},
+	Short:   "(unsafe) Remove all the data and WAL, reset this node's validator to genesis state",
+	RunE:    resetAllCmd,
+}
+var keepAddrBook bool
+
+// ResetStateCmd removes the database of the specified CometBFT core instance.
+var ResetStateCmd = &cobra.Command{
+	Use:     "reset-state",
+	Aliases: []string{"reset_state"},
+	Short:   "Remove all the data and WAL",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		defaultConfig, err = ParseConfig(cmd)
+		if err != nil {
+			return err
+		}
+
+		return resetState(defaultConfig.DBDir(), logger)
+	},
+}
+
+func init() {
+	ResetAllCmd.Flags().BoolVar(&keepAddrBook, "keep-addr-book", false, "keep the address book intact")
+}
+
+// XXX: this is totally unsafe.
+// it's only suitable for testnets.
+func resetAllCmd(cmd *cobra.Command, args []string) (err error) {
+	defaultConfig, err = ParseConfig(cmd)
+	if err != nil {
+		return err
+	}
+
+	return resetAll(
+		defaultConfig.DBDir(),
+		defaultConfig.P2P.AddrBookFile(),
+		defaultConfig.PrivValidatorKeyFile(),
+		defaultConfig.PrivValidatorStateFile(),
+		logger,
+	)
+}
+
+// XXX: this is totally unsafe.
+// it's only suitable for testnets.
+func resetPrivValidator(cmd *cobra.Command, args []string) (err error) {
+	defaultConfig, err = ParseConfig(cmd)
+	if err != nil {
+		return err
+	}
+
+	resetFilePV(defaultConfig.PrivValidatorKeyFile(), defaultConfig.PrivValidatorStateFile(), logger)
+	return nil
+}
+
+func resetFilePV(privValKeyFile, privValStateFile string, logger tmlog.Logger) {
+	if _, err := os.Stat(privValKeyFile); err == nil {
+		pv := privval.LoadFilePVEmptyState(privValKeyFile, privValStateFile)
+		pv.Reset()
+		logger.Info(
+			"Reset private validator file to genesis state",
+			"keyFile", privValKeyFile,
+			"stateFile", privValStateFile,
+		)
+	} else {
+		pv := privval.GenFilePV(privValKeyFile, privValStateFile)
+		pv.Save()
+		logger.Info(
+			"Generated private validator file",
+			"keyFile", privValKeyFile,
+			"stateFile", privValStateFile,
+		)
+	}
+}
+
+func removeAddrBook(addrBookFile string, logger tmlog.Logger) {
+	if err := os.Remove(addrBookFile); err == nil {
+		logger.Info("Removed existing address book", "file", addrBookFile)
+	} else if !os.IsNotExist(err) {
+		logger.Info("Error removing address book", "file", addrBookFile, "err", err)
+	}
+}
+
+// resetAll removes address book files plus all data, and resets the privValdiator data.
+func resetAll(dbDir, addrBookFile, privValKeyFile, privValStateFile string, logger tmlog.Logger) error {
+	if keepAddrBook {
+		logger.Info("The address book remains intact")
+	} else {
+		removeAddrBook(addrBookFile, logger)
+	}
+
+	if err := os.RemoveAll(dbDir); err == nil {
+		logger.Info("Removed all blockchain history", "dir", dbDir)
+	} else {
+		logger.Error("Error removing all blockchain history", "dir", dbDir, "err", err)
+	}
+
+	if err := tmos.EnsureDir(dbDir, 0700); err != nil {
+		logger.Error("unable to recreate dbDir", "err", err)
+	}
+
+	// recreate the dbDir since the privVal state needs to live there
+	resetFilePV(privValKeyFile, privValStateFile, logger)
+	return nil
+}
+
+// resetState removes address book files plus all databases.
+func resetState(dbDir string, logger tmlog.Logger) error {
+	blockdb := filepath.Join(dbDir, "blockstore.db")
+	state := filepath.Join(dbDir, "state.db")
+	wal := filepath.Join(dbDir, "cs.wal")
+	evidence := filepath.Join(dbDir, "evidence.db")
+	txIndex := filepath.Join(dbDir, "tx_index.db")
+
+	if tmos.FileExists(blockdb) {
+		if err := os.RemoveAll(blockdb); err == nil {
+			logger.Info("Removed all blockstore.db", "dir", blockdb)
+		} else {
+			logger.Error("error removing all blockstore.db", "dir", blockdb, "err", err)
+		}
+	}
+
+	if tmos.FileExists(state) {
+		if err := os.RemoveAll(state); err == nil {
+			logger.Info("Removed all state.db", "dir", state)
+		} else {
+			logger.Error("error removing all state.db", "dir", state, "err", err)
+		}
+	}
+
+	if tmos.FileExists(wal) {
+		if err := os.RemoveAll(wal); err == nil {
+			logger.Info("Removed all cs.wal", "dir", wal)
+		} else {
+			logger.Error("error removing all cs.wal", "dir", wal, "err", err)
+		}
+	}
+
+	if tmos.FileExists(evidence) {
+		if err := os.RemoveAll(evidence); err == nil {
+			logger.Info("Removed all evidence.db", "dir", evidence)
+		} else {
+			logger.Error("error removing all evidence.db", "dir", evidence, "err", err)
+		}
+	}
+
+	if tmos.FileExists(txIndex) {
+		if err := os.RemoveAll(txIndex); err == nil {
+			logger.Info("Removed tx_index.db", "dir", txIndex)
+		} else {
+			logger.Error("error removing tx_index.db", "dir", txIndex, "err", err)
+		}
+	}
+
+	if err := tmos.EnsureDir(dbDir, 0700); err != nil {
+		logger.Error("unable to recreate dbDir", "err", err)
+	}
+	return nil
 }
